94ab6e0e6836c35f972e69a5aefc12a233dcc2de
Erick Oduniyi <31193655+eoduniyi@users.noreply.github.com>
2020-05-18 09:43:17 -0500

    Add main.R

---
+++ main.R
@@ -0,0 +1,7 @@
+# Install hyperSpec, covr, and lintr packages from CRAN
+install.packages("hyperSpec")
+install.packages("lintr")
+install.packages("covr")
+
+# Install hyperSpec.tidyverse package from github
+remotes::install_github("cbeleites/hyperSpec.tidyverse")
